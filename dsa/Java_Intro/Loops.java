873a3c52d5154016da89ea9f272bea90cdd6add9
R1DarkEye <anonymous172q@gmail.com>
2026-04-12 19:44:37 +0530

    Update Java intro loops and variables examples

--- dsa/Java_Intro/Loops.java
+++ dsa/Java_Intro/Loops.java
@@ -1,14 +1,21 @@
 class Loops{
     public static void main(String[] args) {
+        int a = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
-        while (true) { 
+        while (a < 5) { 
             System.out.println("its true");
-            return;
+            a++;
+            if (a == 3) break; // exit loop when a is 3
         }
+        do {
+            System.out.println("a is " + a);
+            a++;
+            
+        } while (a<5);
     }
 }
